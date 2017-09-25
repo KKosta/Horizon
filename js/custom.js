9494a33633d0727232d8a7cd700bbf1e2bef884d
Kostadin Kolev <kolev1993@hotmail.co.uk>
2017-09-25 11:35:26 +0100

    New Infographic + Video Update

--- js/custom.js
+++ js/custom.js
@@ -36,6 +36,12 @@ $(document).ready(function () {
 
 	});
 
+	$("#scroll_down").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $("#infographic").offset().top},
+	        'slow');
+	});
+
 	// $(".agenda_bigger").on("click", function () {
 	// 	$("#bigger").fadeIn(500);
 	// });
